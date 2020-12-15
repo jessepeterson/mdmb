72d061882533f669ecd06330cfc3dd1bcfb5fb27
Jesse Peterson <jesse.c.peterson@gmail.com>
2020-12-14 23:04:10 -0800

    profile parse & scep WIP

--- cmd/mdmb/main.go
+++ cmd/mdmb/main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
+	stdlog "log"
 	"os"
+
+	"github.com/go-kit/kit/log"
+	"github.com/groob/plist"
+	"github.com/jessepeterson/cfgprofiles"
+	scepclient "github.com/micromdm/scep/client"
 )
 
 func main() {
 	f := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
-	var (
-		dbPath = f.String("db", "mdmb.db", "mdmb database file path")
-	)
+	// var (
+	// 	dbPath = f.String("db", "mdmb.db", "mdmb database file path")
+	// )
 	f.Usage = func() {
 		fmt.Fprintf(f.Output(), "%s [flags] <subcommand> [flags]\n", f.Name())
 		fmt.Fprint(f.Output(), "\nFlags:\n")
@@ -28,7 +35,9 @@ func main() {
 
 	switch f.Args()[0] {
 	case "enroll":
-		enroll(f.Args()[1:], *dbPath, f.Usage)
+		enroll(f.Args()[1:], f.Usage)
+	case "help":
+		f.Usage()
 	default:
 		fmt.Fprintf(f.Output(), "invalid subcommand: %s\n", f.Args()[0])
 		f.Usage()
@@ -36,18 +45,79 @@ func main() {
 	}
 }
 
-func enroll(args []string, _ string, mainUsage func()) {
+func enroll(args []string, usage func()) {
 	f := flag.NewFlagSet("enroll", flag.ExitOnError)
 	var (
-		enrollType = f.String("type", "profile", "enrollment type")
-		number     = f.Int("n", 1, "number of devices")
+		// enrollType = f.String("type", "profile", "enrollment type")
+		// number     = f.Int("n", 1, "number of devices")
+		url  = f.String("url", "", "URL pointing to enrollment spec (e.g. profile)")
+		file = f.String("file", "", "file of enrollment spec (e.g. profile)")
 	)
 	f.Usage = func() {
-		mainUsage()
-		fmt.Fprintf(f.Output(), "\n%s subcommand flags:\n", f.Name())
+		usage()
+		fmt.Fprintf(f.Output(), "\nFlags for %s subcommand:\n", f.Name())
 		f.PrintDefaults()
 	}
 	f.Parse(args)
 
-	fmt.Printf("enrollment type: %s\nnumber: %d\n", *enrollType, *number)
+	if (*url == "" && *file == "") || (*url != "" && *file != "") {
+		fmt.Fprintln(f.Output(), "must specify one enrollment url or file")
+		f.Usage()
+		os.Exit(2)
+	}
+
+	if *url != "" {
+		fmt.Fprintln(f.Output(), "-url not yet supported")
+		os.Exit(1)
+	}
+
+	if err := enrollWithFile(*file); err != nil {
+		stdlog.Fatal(err)
+	}
+
+	// c := client.NewMDMClient()
+	// fmt.Println(c.UDID)
+}
+
+func enrollWithFile(path string) error {
+
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	profile := &cfgprofiles.Profile{}
+
+	dec := plist.NewDecoder(f)
+	if err := dec.Decode(profile); err != nil {
+		return err
+	}
+
+	mdmPlds := profile.MDMPayloads()
+	if len(mdmPlds) != 1 {
+		return errors.New("invalid number of MDM payloads")
+	}
+	mdmPld := mdmPlds[0]
+
+	fmt.Printf("CheckIn:\t%s\nConnect:\t%s\n", mdmPld.CheckInURL, mdmPld.ServerURL)
+
+	scepPlds := profile.SCEPPayloads()
+	if len(mdmPlds) != 1 {
+		return errors.New("invalid number of MDM payloads")
+	}
+	scepPld := scepPlds[0]
+
+	scepUrl := scepPld.PayloadContent.URL
+	fmt.Printf("SCEP URL:\t%s\n", scepUrl)
+
+	logger := log.NewLogfmtLogger(os.Stderr)
+	logger = log.With(logger, "ts", log.DefaultTimestampUTC)
+	cl, err := scepclient.New(scepUrl, logger)
+	if err != nil {
+		return err
+	}
+	fmt.Println(cl.Supports("POSTPKIOperation"))
+
+	return nil
 }
---
+++ go.mod
@@ -0,0 +1,14 @@
+module github.com/jessepeterson/mdmb
+
+go 1.13
+
+require (
+	github.com/go-kit/kit v0.10.0
+	github.com/google/uuid v1.1.2
+	github.com/groob/plist v0.0.0-20190114192801-a99fbe489d03
+	github.com/jessepeterson/cfgprofiles v0.0.0-20201209172704-598005e95a89
+	github.com/micromdm/scep v1.0.1-0.20201110133952-f3adbb75202b
+	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
+)
+
+replace github.com/jessepeterson/cfgprofiles => ../../../../../cfgprofiles
